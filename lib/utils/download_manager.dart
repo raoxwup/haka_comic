7d4fcf8ea48e70e4074bf08cb53d0307d48e9f72
raoxiongwen <raoxiongwen@eelink.com.cn>
2025-05-16 17:37:06 +0800

    增加我的下载页面

--- lib/utils/download_manager.dart
+++ lib/utils/download_manager.dart
@@ -9,26 +9,43 @@ import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// 下载隔离区入口点
 void _downloadIsolateEntry((SendPort, RootIsolateToken) args) {
   final (sendPort, rootToken) = args;
   BackgroundIsolateBinaryMessenger.ensureInitialized(rootToken);
 
   final receivePort = ReceivePort();
   sendPort.send(receivePort.sendPort);
+  _ComicDownloader.mainIsolateSendPort = sendPort;
 
   receivePort.listen((message) {
     if (message is ComicDownloadTask) {
       _ComicDownloader.add(message);
+    } else if (message is _DownloadCommand) {
+      _ComicDownloader.handleCommand(message);
     }
   });
 }
 
+/// 下载命令类型
+enum _CommandType { pause, resume, cancel, query }
+
+/// 下载命令
+class _DownloadCommand {
+  final String comicId;
+  final _CommandType type;
+
+  const _DownloadCommand({required this.comicId, required this.type});
+}
+
+/// 漫画下载器
 class _ComicDownloader {
   static final List<ComicDownloadTask> tasks = [];
-  static final dio = Dio();
-
+  static final Dio dio = Dio();
   static final Map<String, CancelToken> _cancelTokens = <String, CancelToken>{};
+  static late final SendPort mainIsolateSendPort;
 
+  /// 下载单张图片
   static Future<void> _downloadImage(
     String url,
     String path,
@@ -46,14 +63,17 @@ class _ComicDownloader {
     }
   }
 
+  /// 下载任务
   static Future<void> download(ComicDownloadTask task) async {
-    int i = 0;
     final dirPath = (await getApplicationDocumentsDirectory()).path;
+    int i = 0;
+
     for (var chapter in task.chapters) {
       for (var image in chapter.images) {
         if (i++ < task.completed) {
           continue;
         }
+
         final path = p.join(
           dirPath,
           'comics',
@@ -61,12 +81,16 @@ class _ComicDownloader {
           sanitizeFileName(chapter.title),
           image.originalName,
         );
+
         await _downloadImage(image.url, path, _cancelTokens[task.comic.id]);
         task.completed++;
+
+        notify();
       }
     }
   }
 
+  /// 初始化章节数据
   static Future<void> chapterInitialize(
     DownloadChapter chapter,
     String id,
@@ -77,7 +101,7 @@ class _ComicDownloader {
     final token = prefs.getString('token');
 
     if (token == null) {
-      throw Exception('token is null');
+      throw Exception('Token is null');
     }
 
     final response = await fetchChapterImagesIsolate(
@@ -87,6 +111,7 @@ class _ComicDownloader {
     chapter.images.addAll(response.map((e) => e.media));
   }
 
+  /// 更新任务
   static Future<void> update(ComicDownloadTask task) async {
     task.total = 0;
     for (var chapter in task.chapters) {
@@ -96,6 +121,7 @@ class _ComicDownloader {
     download(task);
   }
 
+  /// 添加下载任务
   static void add(ComicDownloadTask task) {
     final index = tasks.indexWhere((t) => task.comic.id == t.comic.id);
     if (index != -1) {
@@ -108,18 +134,48 @@ class _ComicDownloader {
     }
   }
 
-  static void pause(ComicDownloadTask task) {
-    _cancelTokens[task.comic.id]?.cancel();
-    _cancelTokens[task.comic.id] = CancelToken();
+  /// 暂停下载任务
+  static void pause(String comicId) {
+    _cancelTokens[comicId]?.cancel();
+    _cancelTokens[comicId] = CancelToken();
+  }
+
+  /// 查询下载任务
+  static void notify() => mainIsolateSendPort.send(tasks);
+
+  /// 处理下载命令
+  static void handleCommand(_DownloadCommand command) {
+    switch (command.type) {
+      case _CommandType.query:
+        notify();
+        break;
+      case _CommandType.pause:
+        pause(command.comicId);
+        break;
+      case _CommandType.resume:
+        final index = tasks.indexWhere((t) => command.comicId == t.comic.id);
+        if (index != -1) {
+          update(tasks[index]);
+        }
+        break;
+      case _CommandType.cancel:
+        pause(command.comicId);
+        tasks.removeWhere((t) => t.comic.id == command.comicId);
+        break;
+    }
   }
 }
 
+/// 下载管理器
 class DownloadManager {
   static final ReceivePort _mainReceivePort = ReceivePort();
   static late final Isolate _workerIsolate;
   static late final SendPort _workerSendPort;
   static final _rootToken = RootIsolateToken.instance!;
+  static final streamController =
+      StreamController<List<ComicDownloadTask>>.broadcast();
 
+  /// 初始化下载管理器
   static Future<void> initialize() async {
     final completer = Completer<void>();
 
@@ -132,22 +188,54 @@ class DownloadManager {
       if (message is SendPort) {
         _workerSendPort = message;
         completer.complete();
+      } else if (message is List<ComicDownloadTask>) {
+        streamController.add(message);
       }
     });
 
     return completer.future;
   }
 
+  // 查询下载任务
+  static void getTasks() => _workerSendPort.send(
+    const _DownloadCommand(type: _CommandType.query, comicId: ''),
+  );
+
+  /// 添加下载任务
   static void addTask(ComicDownloadTask task) {
     _workerSendPort.send(task);
   }
 
+  /// 暂停下载任务
+  static void pauseTask(String comicId) {
+    _workerSendPort.send(
+      _DownloadCommand(comicId: comicId, type: _CommandType.pause),
+    );
+  }
+
+  /// 恢复下载任务
+  static void resumeTask(String comicId) {
+    _workerSendPort.send(
+      _DownloadCommand(comicId: comicId, type: _CommandType.resume),
+    );
+  }
+
+  /// 取消下载任务
+  static void cancelTask(String comicId) {
+    _workerSendPort.send(
+      _DownloadCommand(comicId: comicId, type: _CommandType.cancel),
+    );
+  }
+
+  /// 释放资源
   static void dispose() {
     _mainReceivePort.close();
     _workerIsolate.kill();
+    streamController.close();
   }
 }
 
+/// 漫画下载任务
 class ComicDownloadTask {
   final DownloadComic comic;
   final List<DownloadChapter> chapters;
@@ -158,6 +246,7 @@ class ComicDownloadTask {
   ComicDownloadTask({required this.comic, required this.chapters});
 }
 
+/// 下载章节
 class DownloadChapter extends Chapter {
   /// 这个章节的所有图片
   final List<ImageDetail> images = [];
@@ -171,6 +260,7 @@ class DownloadChapter extends Chapter {
   });
 }
 
+/// 下载漫画
 class DownloadComic {
   final String id;
   final String title;
--- lib/views/mine/downloads.dart
+++ lib/views/mine/downloads.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:haka_comic/utils/download_manager.dart';
+import 'package:haka_comic/utils/extension.dart';
+import 'package:haka_comic/utils/ui.dart';
+import 'package:haka_comic/widgets/base_image.dart';
 
 class Downloads extends StatefulWidget {
   const Downloads({super.key});
@@ -8,8 +12,82 @@ class Downloads extends StatefulWidget {
 }
 
 class _DownloadsState extends State<Downloads> {
+  List<ComicDownloadTask> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    DownloadManager.streamController.stream.listen(
+      (event) => setState(() {
+        tasks = event;
+      }),
+    );
+    DownloadManager.getTasks();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('下载')));
+    final width = context.width;
+    return Scaffold(
+      appBar: AppBar(title: const Text('下载')),
+      body: CustomScrollView(
+        slivers: [
+          SliverGrid.builder(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent:
+                  UiMode.m1(context)
+                      ? width
+                      : UiMode.m2(context)
+                      ? width / 2
+                      : width / 3,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+              childAspectRatio: 2.5,
+            ),
+            itemBuilder: (context, index) {
+              final task = tasks[index];
+              return SizedBox(
+                key: ValueKey(task.comic.id),
+                width: double.infinity,
+                height: 170,
+                child: Row(
+                  spacing: 8,
+                  children: [
+                    BaseImage(url: task.comic.cover, aspectRatio: 13 / 17),
+                    Expanded(
+                      child: Column(
+                        spacing: 5,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            task.comic.title,
+                            style: context.textTheme.titleSmall,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const Spacer(),
+                          LinearProgressIndicator(
+                            value:
+                                task.total == 0
+                                    ? null
+                                    : task.completed / task.total,
+                            semanticsLabel:
+                                task.total == 0
+                                    ? 'Loading...'
+                                    : '${task.completed} / ${task.total}',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            itemCount: tasks.length,
+          ),
+        ],
+      ),
+    );
   }
 }
