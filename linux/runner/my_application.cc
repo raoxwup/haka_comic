2ee9f21048b4997b22ae08e901d4801bfc577ae5
rxw <2587897409@qq.com>
2026-01-22 16:30:06 +0800

    测试给linux增加上icon

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -54,6 +54,8 @@ static void my_application_activate(GApplication* application) {
 
   gtk_window_set_default_size(window, 380, 720);
 
+  gtk_window_set_icon_from_file(GTK_WINDOW(window),"assets/icons/pc/linux_icon.png",NULL);
+
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(
       project, self->dart_entrypoint_arguments);
