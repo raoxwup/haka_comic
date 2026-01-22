acdd5cf07985c6485ee52fbe4ecba701e9276716
raoxiongwen <2587897409@qq.com>
2026-01-22 21:59:20 +0800

    继续测试linux图标问题

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -25,12 +25,6 @@ static void my_application_activate(GApplication* application) {
 
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
-
-  const string iconFilename = "assets/icons/pc/linux_icon.png";
-  path execDir = canonical(read_symlink("/proc/self/exe")).parent_path();
-  path iconPath = execDir / "data/flutter_assets" / iconFilename;
-  gtk_window_set_icon_from_file(GTK_WINDOW(window), iconPath.c_str(), NULL);
-
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
   // desktop).
