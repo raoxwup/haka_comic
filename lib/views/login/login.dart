cca3baeea659b56bf60765dbd06fa58bd286923a
raoxwup <2587897409@qq.com>
2026-05-25 14:56:03 +0800

    图标还原 #156

--- lib/views/login/login.dart
+++ lib/views/login/login.dart
@@ -78,10 +78,7 @@ class _LoginState extends State<Login> with RequestMixin {
                 spacing: 20,
                 children: [
                   SizedBox(height: isDesktop ? 30 : 70),
-                  Image.asset(
-                    'assets/icons/android/ic_launcher_foreground.png',
-                    width: 180,
-                  ),
+                  Image.asset('assets/images/foreground.png', width: 180),
                   _buildLoginForm(),
                 ],
               ),
